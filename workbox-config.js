9bb0ee241af9723ed24f2e9720b394a3cbc73aed
Lucky Victory <luckyvictory54@gmail.com>
2023-04-25 00:44:10 +0100

    Update workbox-config.js

--- workbox-config.js
+++ workbox-config.js
@@ -1,8 +1,28 @@
 module.exports = {
 	globDirectory: 'www/',
 	globPatterns: [
-		'**/*.{woff2,woff,js,css,png,html}'
+		'**/*.{html}'
 	],
+runtimeCaching: [
+    {
+      urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'images',
+        expiration: {
+          maxEntries: 10,
+          maxAgeSeconds: 60 * 60 * 24 * 30, // 30 Days
+        },
+      },
+    },
+    {
+      urlPattern: /\.(?:css|js)$/,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'assets',
+      },
+    }
+  ],
 	swDest: 'www/sw.js',
 	ignoreURLParametersMatching: [
 		/^utm_/,
