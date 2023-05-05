2dfc57ca95655a6597da1a07c3ff80351efbcaa1
Lucky-victory <blizwonder25@gmail.com>
2023-05-05 05:57:55 +0100

    added android pp exit

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,17 +1,20 @@
 import React, { useRef, useState } from "react";
 import { f7, f7ready, App, View } from "framework7-react";
-
+import {getDevice} from 'framework7'
 import routes from "../js/routes";
 import store from "../js/store";
 import { CalculatorContext } from "../context/calculator";
+import { App as CapacitorApp } from '@capacitor/app';
 
+import {capacitorApp } from '../js/capacitor'
+const device=getDevice();
 const MyApp = () => {
   // Framework7 Parameters
   const f7params = {
     name: "Calculator", // App name
     theme: "auto", // Automatic theme detection
     colors: {
-      primary: "#0d887e",
+      primary: "#09b1a3",
     },
     touch: {
       tapHold: true, //enable tap hold events
@@ -24,7 +27,17 @@ const MyApp = () => {
 
   f7ready(() => {
     // Call F7 APIs here
+    if(device.capacitor) {
+
+      capacitorApp.init(f7)
+      document.addEventListener('backbutton',()=>{handleAppExit()})
+    }
   });
+
+  function handleAppExit(){
+    
+    CapacitorApp.exitApp()
+  }
   const inputRef = useRef();
   const [state, setState] = useState({
     caretPosition: -1,
